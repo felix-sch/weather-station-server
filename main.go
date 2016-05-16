1f1288ed66e36a7d09aa61345fa28c6c1e08dbe6
Felix Schaumann <felix.schaumann@de.ibm.com>
2016-05-16 04:04:10 +0200

    Add Gorilla URL router

--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func ApiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "API is alive.")
+
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	if len(key) > 0 {
+		fmt.Fprintf(w, "\nKey: %s", key)
+	}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./app/index.html")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+
+	s := r.PathPrefix("/api").Subrouter()
+	s.HandleFunc("/", ApiHandler).Methods("GET")
+	s.HandleFunc("/{key}/", ApiHandler)
+
+	r.HandleFunc("/", HomeHandler)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./app/")))
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
